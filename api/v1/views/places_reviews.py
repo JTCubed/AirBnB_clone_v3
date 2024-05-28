efec56d3f1fe7fa9dd52bd32d58f9f65d999d3d5
JTCubed <zeusio60@gmail.com>
2024-05-28 16:37:10 -0700

    hot fix

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -23,7 +23,7 @@ def get_place_r(place_id):
     return jsonfy(lis.to_dict), 200
 
 
-@app_views.route("/reviews/<review_id>",
+@app_views.route('/reviews/<review_id>',
                  methods=["GET"], strict_slashes=False)
 def get_rev(review_id):
     """return a review or 404 if not found"""
